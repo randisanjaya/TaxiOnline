d68fb1c86c920a873e635f274fd21652eb1279e7
randisanjaya <randi.sanjaya@students.amikom.ac.id>
2022-03-23 14:31:10 +0700

    C# Aplikasi Taxi Online

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxionline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new taxionline.Taxi();
+
+            taxi.drivername = "Aika";
+            taxi.onduty = true;
+            taxi.passenggernum = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassengger();
+            taxi.DropOffPassengger();
+        }
+    }
+}
---
+++ taxi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxionline
+{
+    public class Taxi
+    {
+        public string drivername { get; set; }
+        public bool onduty { get; set; }
+        public int passenggernum { get; set; }
+
+        public void TaxiInfo ()
+        {
+            Console.WriteLine("Driver Name: {0}", drivername);
+            Console.WriteLine("On Duty: {0}", onduty);
+            Console.WriteLine("Number of Passengger: {0}", passenggernum);
+            Console.WriteLine();
+        }
+
+        public void PickUpPassengger ()
+        {
+            Console.WriteLine("{0} sedang menjemput Penumpang", drivername);
+        }
+
+        public void DropOffPassengger ()
+        {
+           Console.WriteLine("{0} sedang mengantar Penumpang", drivername);
+        }
+    }
+}
